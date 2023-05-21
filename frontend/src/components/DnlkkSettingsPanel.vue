28e0fab8b294473d8dd1807462e86489152efd17
danilkk <ezzfvkoko@gmail.com>
2023-05-21 23:04:05 +0300

    frontend: add the possibility to change tabs

--- frontend/src/components/DnlkkSettingsPanel.vue
+++ frontend/src/components/DnlkkSettingsPanel.vue
@@ -1,17 +1,25 @@
 <script>
 import {defineComponent} from 'vue'
 import DnlkkColor from "./editor/DnlkkColor.vue";
+import DnlkkSize from "./editor/DnlkkSize.vue";
+import DnlkkCompress from "./editor/DnlkkCompress.vue";
+import DnlkkPrikols from "./editor/DnlkkPrikols.vue";
 
 export default defineComponent({
   name: "DnlkkSettingsPanel",
-  components: {DnlkkColor},
+  components: {
+    DnlkkPrikols,
+    DnlkkCompress, DnlkkSize, DnlkkColor},
   props: ['method'],
 })
 </script>
 
 <template>
   <v-container>
-    <dnlkk-color/>
+    <dnlkk-color v-if="method === 'Цвет'"/>
+    <dnlkk-size v-else-if="method === 'Размер'" />
+    <dnlkk-compress v-else-if="method === 'Сжатие'"/>
+    <dnlkk-prikols v-else/>
   </v-container>
 </template>
 
